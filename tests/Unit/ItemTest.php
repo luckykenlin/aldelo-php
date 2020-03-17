f3752308f6620dde2e695b54aad5f43ed14a15ea
ken <luckykenlin@gmail.com>
2020-03-17 14:07:19 -0400

    update composer json

--- tests/Unit/ItemTest.php
+++ tests/Unit/ItemTest.php
@@ -10,12 +10,11 @@ class ItemTest extends TestCase
 {
     public function setUpEnv()
     {
-        Aldelo::setIsvId(env("ALDELO_ISV_ID", 'D-181207-0001'));
-        Aldelo::setIsvKey(env("ALDELO_ISV_KEY", '480a31cb-03e6-4718-9e16-2d7a27e7af8f'));
-        Aldelo::setAppKey(env("ALDELO_APP_KEY", '6eeeccfb-dd19-41a3-b2fa-a15586c23e64'));
-        Aldelo::setAppVersion(env("ALDELO_APP_VERSION", '1.0.0.0'));
-        Aldelo::setStoreSubId(env("ALDELO_STORE_SUB_ID", '2296-1C2A'));
-        Aldelo::setStoreSubId(env("ALDELO_STORE_APP_TOKEN", '72ce5c21-9885-4de8-9f07-7dcc3202e83a'));
+        Aldelo::setIsvId('D-181207-0001');
+        Aldelo::setIsvKey('480a31cb-03e6-4718-9e16-2d7a27e7af8f');
+        Aldelo::setAppKey('6eeeccfb-dd19-41a3-b2fa-a15586c23e64');
+        Aldelo::setAppVersion('1.0.0.0');
+        Aldelo::setStoreSubId('2296-1C2A');
     }
 
     /**
